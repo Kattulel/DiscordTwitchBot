9fe1cfceacc586cc9ce38c5977aac96f727f4727
Gabriel <61303132+Kattulel@users.noreply.github.com>
2020-03-03 20:26:40 -0300

    adding stuff

--- main.py
+++ main.py
@@ -2,9 +2,15 @@
 import twitch
 import asyncio
 import key
+import discord
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix='!twitchb ')
 
 helix = twitch.Helix(key.client_id)
 
+connections = []
+stream_channel = None
 userlist = list()
 messages = {"sent": list()}
 
@@ -13,7 +19,37 @@ f = open("channels.txt", "r")
 channels = f.read().split(",")
 f.close()
 for name in channels:
-    userlist.append(name)
+    if name != "":
+        userlist.append(name)
+
+
+@bot.event
+async def on_ready():
+    global connections
+    global stream_channel
+    connections = bot.guilds
+    channels = connections[0].channels
+
+    # find channel
+    for i in channels:
+        if i.name.lower() == "stream":
+            stream_channel = connections[0].get_channel(i.id)  # check if found anyway;
+            break
+    await run()
+
+
+# test for name changes
+@bot.command()
+async def name(ctx, newname):
+    await stream_channel.edit(name=newname)
+
+
+@bot.command()
+async def notify(ctx, channel_name):
+    f = open("channels.txt", "a+")
+    f.write(channel_name+",")
+    f.close()
+    await ctx.send("Notify OK")
 
 
 def alreadySent(username):
@@ -31,15 +67,14 @@ def sendMessage(username):
 
 def checkLive():
     for i in userlist:
-        sendMessage(i)
+        user = helix.user(i)
+        if user.is_live:
+            sendMessage(i)
 
 
 async def run():
-    while True:
-        checkLive()
-        await asyncio.sleep(5)
+    checkLive()
+    await asyncio.sleep(25)
+    await run()
 
-if __name__ == '__main__':
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(run())
-
+bot.run(key.bot_token)
